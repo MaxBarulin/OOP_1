7aa8663edd8815dcfb0ccfae3d6e0adcf6914c96
Max Barulin <barulin.max@gmail.com>
2024-10-27 22:31:59 +0300

    17.1 дороаботка тестов(1)

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.product import Product
 from src.exception_quantity import ExceptionQuantity
 
@@ -6,6 +7,10 @@ from src.exception_quantity import ExceptionQuantity
 def test_init_product(product1, product2):
     assert product1.name == "Samsung Galaxy S23 Ultra"
     assert product1.description == "256GB, Серый цвет, 200MP камера"
+    assert product1.price == 180000.0
+    assert product1.quantity == 5
+
+    assert product2.name == "Iphone 15"
     assert product2.description == "512GB, Gray space"
     assert product2.price == 210000.0
     assert product2.quantity == 8
